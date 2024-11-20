46b77cf5e98059ec8973e0b2757272c9f6fb2f05
mahendra <solankirakesh895@gmail.com>
2024-11-20 14:37:29 +0530

    improved: UI text of skill match

--- client/src/pages/Aboutjob/SkillMatcher.jsx
+++ client/src/pages/Aboutjob/SkillMatcher.jsx
@@ -9,16 +9,16 @@ const SkillMatcher = ({ userskills, jobskills }) => {
   let textColor;
 
   if (percentage === 100) {
-    textContent = 'Congratulations, your skills perfectly match this job!';
+    textContent = 'your skills perfectly match this job! Match score';
     textColor = 'text-green-500';
   } else if (percentage >= 70) {
-    textContent = 'Great, your skills match this job well!';
+    textContent = 'your skills match this job well! Match score';
     textColor = 'text-green-500';
   } else if (percentage >= 40) {
-    textContent = 'Your skills match this job, but there is room for improvement.';
+    textContent = 'Your skills match this job, but need for improvement. Match score';
     textColor = 'text-yellow-500';
   } else {
-    textContent = 'Unfortunately, your skills do not match this job.';
+    textContent = ' your skills do not match this job. Match score';
     textColor = 'text-red-500';
   }
 
