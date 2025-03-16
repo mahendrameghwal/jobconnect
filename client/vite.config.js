cdc99e47f1efb89d7a97fec389728e1745bec374
mahendra kumar <solankirakesh895@gmail.com>
2025-03-16 11:59:31 +0530

    increase Node.js heap memory limit for Netlify deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,31 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginRewriteAll from 'vite-plugin-rewrite-all'
-import removeConsole from 'vite-plugin-remove-console'
-import path from 'path'
-
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import removeConsole from 'vite-plugin-remove-console';
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    pluginRewriteAll(),
-    removeConsole({ exclude: ['error', 'warn'] })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        }
-      }
-    }
-  }
+  plugins: [react(), pluginRewriteAll(),  removeConsole({ exclude: ['error', 'warn'] })]
 })
